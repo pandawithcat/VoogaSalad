7d7ba0b19b6376920594fecbb4acdc42626c17bc
dl199@duke.edu <dl199@duke.edu>
2019-04-03 11:43:37 -0400

    drag and drop all implemented

--- src/GUI/GameAuthoringEnvironment/AuthoringScreen/Modules/Editors/ArsenalEditor.java
+++ src/GUI/GameAuthoringEnvironment/AuthoringScreen/Modules/Editors/ArsenalEditor.java
@@ -12,6 +12,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,29 +21,54 @@ import java.util.List;
 //
 public class ArsenalEditor extends Module {
 
-    ListView<Arsenal> sourceView = new ListView<Arsenal>();
-    ListView<Arsenal> targetView = new ListView<Arsenal>();
-    TextArea loggingArea = new TextArea("");
-    static final DataFormat Arsenal_LIST = new DataFormat("Arsenal List");
+    private final int sourceViewWidth = 200;
+    private final int sourceViewHeight = 200;
+    private final int viewGap = 10;
+    private ObservableList<Arsenal> arsenalList = FXCollections.observableArrayList();
+    private ListView<Arsenal> sourceView = new ListView<>();
+    private ListView<Arsenal> targetView = new ListView<>();
+    private TextArea loggingArea = new TextArea("");
+    private static final DataFormat Arsenal_LIST = new DataFormat("Arsenal List");
 
-    public ArsenalEditor(Group myRoot, int width, int height, String moduleName){
+    public ArsenalEditor(Group myRoot, int width, int height, String moduleName) {
         super(myRoot, width, height, moduleName, true);
-        String cssLayout = "-fx-border-color: red;\n" +
-                "-fx-border-insets: 5;\n" +
-                "-fx-border-width: 3;\n" +
-                "-fx-border-style: dashed;\n";
-
-        getContent().setStyle(cssLayout);
+        setLayout(600, 600);
+        setContentColor(Color.LIGHTBLUE);
+        setContent();
     }
 
-    public void setContent(){
-        Label sourceListLbl = new Label("Source List: ");
-        Label targetListLbl = new Label("Target List: ");
+    //TODO put Arsenal as an argument
+    public void addArsenals(Arsenal arsenal) {
+        arsenalList.addAll(arsenal);
+    }
+
+    private ObservableList<Arsenal> getArsenalList() {
+        return arsenalList;
+    }
+
+    //TODO getter function
+  /*  public ObservableList<Arsenal> getAllArsenalList(){
+
+    }
+
+    public ObservableList<Arsenal> getSelectedArsenalList(){
+
+    }*/
+
+    public void setContent() {
+        Label sourceListLbl = new Label("Available Towers: ");
+        Label targetListLbl = new Label("Selected Towers: ");
         Label messageLbl = new Label("Select arsenals from the given list, drag and drop them to another list");
 
-        sourceView.setPrefSize(200, 200);
-        targetView.setPrefSize(200, 200);
-        loggingArea.setMaxSize(410, 200);
+        sourceView.setPrefSize(sourceViewWidth, sourceViewHeight);
+        targetView.setPrefSize(sourceViewWidth, sourceViewHeight);
+        loggingArea.setMaxSize(sourceViewWidth * 2 + viewGap, sourceViewHeight);
+
+        //examples
+        Arsenal iceTower = new Arsenal("iceTower");
+        Arsenal fireTower = new Arsenal("fireTower");
+        addArsenals(iceTower);
+        addArsenals(fireTower);
 
         sourceView.getItems().addAll(getArsenalList());
 
@@ -51,117 +77,76 @@ public class ArsenalEditor extends Module {
 
         // Create the GridPane
         GridPane pane = new GridPane();
-        pane.setHgap(10);
-        pane.setVgap(10);
+        pane.setHgap(viewGap);
+        pane.setVgap(viewGap);
 
         // Add the Labels and Views to the Pane
         pane.add(messageLbl, 0, 0, 3, 1);
         pane.addRow(1, sourceListLbl, targetListLbl);
         pane.addRow(2, sourceView, targetView);
 
+        setDragAndDrop();
+        VBox root = new VBox();
+        // Add the Pane and The LoggingArea to the VBox
+        root.getChildren().addAll(pane, loggingArea);
+        getContent().getChildren().add(root);
 
-        sourceView.setOnDragDetected(new EventHandler<MouseEvent>()
-        {
-            public void handle(MouseEvent event)
-            {
-                writelog("Event on Source: drag detected");
+    }
+
+    private void setDragAndDrop() {
+
+        sourceView.setOnDragDetected(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                writelog("sourceview dragdetected");
                 dragDetected(event, sourceView);
             }
         });
 
-        sourceView.setOnDragOver(new EventHandler <DragEvent>()
-        {
-            public void handle(DragEvent event)
-            {
-                writelog("Event on Source: drag over");
+        sourceView.setOnDragOver(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                writelog("sourceview draggedover");
                 dragOver(event, sourceView);
             }
         });
 
-        sourceView.setOnDragDropped(new EventHandler <DragEvent>()
-        {
-            public void handle(DragEvent event)
-            {
-                writelog("Event on Source: drag dropped");
-                dragDropped(event, sourceView);
-            }
-        });
-
-        sourceView.setOnDragDone(new EventHandler <DragEvent>()
-        {
-            public void handle(DragEvent event)
-            {
-                writelog("Event on Source: drag done");
+        sourceView.setOnDragDone(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                writelog("sourceview dragdone");
+                dragDropped(event, targetView);
                 dragDone(event, sourceView);
             }
         });
 
         // Add mouse event handlers for the target
-        targetView.setOnDragDetected(new EventHandler <MouseEvent>()
-        {
-            public void handle(MouseEvent event)
-            {
-                writelog("Event on Target: drag detected");
+        targetView.setOnDragDetected(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                writelog("targetview dragdetected");
                 dragDetected(event, targetView);
             }
         });
 
-        targetView.setOnDragOver(new EventHandler <DragEvent>()
-        {
-            public void handle(DragEvent event)
-            {
-                writelog("Event on Target: drag over");
+        targetView.setOnDragOver(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                writelog("targetview draggedover");
                 dragOver(event, targetView);
             }
         });
 
-        targetView.setOnDragDropped(new EventHandler <DragEvent>()
-        {
-            public void handle(DragEvent event)
-            {
-                writelog("Event on Target: drag dropped");
-                dragDropped(event, targetView);
-            }
-        });
-
-        targetView.setOnDragDone(new EventHandler <DragEvent>()
-        {
-            public void handle(DragEvent event)
-            {
+        targetView.setOnDragDone(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
                 writelog("Event on Target: drag done");
+                dragDropped(event, sourceView);
                 dragDone(event, targetView);
             }
         });
-
-
-        VBox root = new VBox();
-        // Add the Pane and The LoggingArea to the VBox
-        root.getChildren().addAll(pane,loggingArea);
-        getContent().getChildren().add(root);
-
-    }
-
-    private ObservableList<Arsenal> getArsenalList()
-    {
-        ObservableList<Arsenal> arsenalList = FXCollections.<Arsenal>observableArrayList();
-
-        Arsenal iceTower = new Arsenal("iceTower");
-        Arsenal fireTower = new Arsenal("fireTower");
-
-
-        arsenalList.addAll(iceTower, fireTower);
-
-        return arsenalList;
     }
 
 
-    private void dragDetected(MouseEvent event, ListView<Arsenal> listView)
-    {
+    private void dragDetected(MouseEvent event, ListView<Arsenal> listView) {
         // Make sure at least one item is selected
         int selectedCount = listView.getSelectionModel().getSelectedIndices().size();
 
-        if (selectedCount == 0)
-        {
+        if (selectedCount == 0) {
             event.consume();
             return;
         }
@@ -170,23 +155,23 @@ public class ArsenalEditor extends Module {
         Dragboard dragboard = listView.startDragAndDrop(TransferMode.COPY_OR_MOVE);
 
         // Put the the selected items to the dragboard
-        ArrayList<Arsenal> selectedItems = this.getSelectedArsenal(listView);
+        ArrayList<Arsenal> selectedItems = getSelectedArsenal(listView);
+        writelog(selectedItems.toString());
 
         ClipboardContent content = new ClipboardContent();
         content.put(Arsenal_LIST, selectedItems);
 
         dragboard.setContent(content);
+        //Stops any further handling of the event
         event.consume();
     }
 
-    private void dragOver(DragEvent event, ListView<Arsenal> listView)
-    {
+    private void dragOver(DragEvent event, ListView<Arsenal> listView) {
         // If drag board has an ITEM_LIST and it is not being dragged
         // over itself, we accept the MOVE transfer mode
         Dragboard dragboard = event.getDragboard();
 
-        if (event.getGestureSource() != listView && dragboard.hasContent(Arsenal_LIST))
-        {
+        if (event.getGestureSource() != listView && dragboard.hasContent(Arsenal_LIST)) {
             event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
         }
 
@@ -194,17 +179,16 @@ public class ArsenalEditor extends Module {
     }
 
     @SuppressWarnings("unchecked")
-    private void dragDropped(DragEvent event, ListView<Arsenal> listView)
-    {
-        boolean dragCompleted = false;
+    private void dragDropped(DragEvent event, ListView<Arsenal> listView) {
 
         // Transfer the data to the target
         Dragboard dragboard = event.getDragboard();
+        boolean dragCompleted = false;
 
-        if(dragboard.hasContent(Arsenal_LIST))
-        {
-            ArrayList<Arsenal> list = (ArrayList<Arsenal>)dragboard.getContent(Arsenal_LIST);
+        if (dragboard.hasContent(Arsenal_LIST)) {
+            ArrayList<Arsenal> list = (ArrayList<Arsenal>) dragboard.getContent(Arsenal_LIST);
             listView.getItems().addAll(list);
+
             // Data transfer is successful
             dragCompleted = true;
         }
@@ -214,14 +198,11 @@ public class ArsenalEditor extends Module {
         event.consume();
     }
 
-    private void dragDone(DragEvent event, ListView<Arsenal> listView)
-    {
-        // Check how data was transfered to the target
-        // If it was moved, clear the selected items
+    private void dragDone(DragEvent event, ListView<Arsenal> listView) {
+
         TransferMode tm = event.getTransferMode();
 
-        if (tm == TransferMode.MOVE)
-        {
+        if (tm == TransferMode.MOVE) {
             removeSelectedArsenals(listView);
         }
 
@@ -229,22 +210,18 @@ public class ArsenalEditor extends Module {
     }
 
 
-    private ArrayList<Arsenal> getSelectedArsenal(ListView<Arsenal> listView)
-    {
-        // Return the list of selected Arsenal in an ArratyList, so it is
-        // serializable and can be stored in a Dragboard.
+    private ArrayList<Arsenal> getSelectedArsenal(ListView<Arsenal> listView) {
+
         ArrayList<Arsenal> list = new ArrayList<>(listView.getSelectionModel().getSelectedItems());
 
         return list;
     }
 
-    private void removeSelectedArsenals(ListView<Arsenal> listView)
-    {
+    private void removeSelectedArsenals(ListView<Arsenal> listView) {
         // Get all selected Arsenal in a separate list to avoid the shared list issue
         List<Arsenal> selectedList = new ArrayList<>();
 
-        for(Arsenal Arsenal : listView.getSelectionModel().getSelectedItems())
-        {
+        for (Arsenal Arsenal : listView.getSelectionModel().getSelectedItems()) {
             selectedList.add(Arsenal);
         }
 
@@ -255,8 +232,7 @@ public class ArsenalEditor extends Module {
     }
 
     // Helper Method for Logging
-    private void writelog(String text)
-    {
+    private void writelog(String text) {
         this.loggingArea.appendText(text + "\n");
     }
 
