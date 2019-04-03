bfe8768d243d668e7b7d5bc97dff56bd39479787
Brian Jordan <bjj17@duke.edu>
2019-04-03 12:47:22 -0400

    Implemented the basic Interfaces for the View and Model of the gameplayer environment

--- src/Logic/LogicController/src/BackendAPI/IModel.java
+++ src/Logic/LogicController/src/BackendAPI/IModel.java
@@ -1,4 +1,15 @@
 package BackendAPI;
 
 public interface IModel {
+
+
+    // Creates Instance of the Game being played
+    // Input 1: Game config object
+    // Input 2: User data Object
+    void createGameInstance();
+
+
+    // Begins game loop
+    // No Input to this function
+    void runGame();
 }
