a5393a5a4d0c85ed333873261a97dbd71e0e4b68
Kunal Upadya <ku16@duke.edu>
2019-04-04 15:33:14 -0400

    fixed class error in configurable

--- src/Config/Configs/src/Configs/Configurable.java
+++ src/Config/Configs/src/Configs/Configurable.java
@@ -1,8 +1,8 @@
 package Configs;
 
-import java.lang.class;
+import java.lang.;
 import java.util.Map;
 
 public interface Configurable {
-    Map<String, java.lang.class> getAttributes();
+    Map<String, Class> getAttributes();
 }
--- src/Config/Configs/src/Configs/View.java
+++ src/Config/Configs/src/Configs/View.java
@@ -1,9 +1,10 @@
 package Configs;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
 public class View {
-
-
     public View(String imageName, int x, int y, int width, int height){
-
+        new ImageView();
     }
 }
