a12f36214479ee4f62082f5c231a8c8a761da1da
Bryant Huang <bh179@duke.edu>
2019-04-03 21:47:44 -0400

    intial screen

---
+++ src/GUI/GamePlay/GameScreen/GameMap.java
@@ -0,0 +1,4 @@
+package GUI.GamePlay.GameScreen;
+
+public class GameMap {
+}
--- src/GUI/GamePlay/GameScreen/GamePlayVisualization.java
+++ src/GUI/GamePlay/GameScreen/GamePlayVisualization.java
@@ -1,4 +1,28 @@
 package GUI.GamePlay.GameScreen;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
 public class GamePlayVisualization {
+    private String Title = "VoogaSalad Game";
+    private static final int FRAMES_PER_SECOND = 1;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+//    private static final Paint backgroundColor = Color.WHITE;
+    private double screenWidth;
+    private double screenHeight;
+    private double screenMinX;
+    private double screenMinY;
+
+    public void start(Stage stage){
+        Stage primaryStage = stage;
+        var root = new Group();
+        var startScreen = new Scene(root, screenWidth, screenHeight);
+        primaryStage.setScene(startScreen);
+        primaryStage.setTitle(Title);
+        primaryStage.show();
+    }
+
 }
