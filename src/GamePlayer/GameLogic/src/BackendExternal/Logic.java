c993cbfb7cf7e3c95989009f493c941f73fed4e3
Mark Kang <sk531@duke.edu>
2019-04-08 00:09:33 -0400

    merge

--- src/GamePlayer/GameLogic/src/BackendExternal/Logic.java
+++ src/GamePlayer/GameLogic/src/BackendExternal/Logic.java
@@ -16,96 +16,96 @@ import java.util.List;
  */
 
 public class Logic {
-////     TODO: Second Sprint
-////     private UserAuthenticator myUserAuthenticator;
-////     private myUserGameData;
-//
-//    private Game myGame;
-//    private GameLibrary myGameLibrary;
-//
-//    public Logic() {
-////        myUserAuthenticator = new UserAuthenticator();
-//        myGameLibrary = new GameLibrary();
-//
-//    }
-//
-//
-//    // View will call this first to get the name and thumbnail file name of each game
-//    // No Input
-//    // Return: List of GameInfo objects
-//    public List<GameInfo> getGameOptions(){
-//        return myGameLibrary.getImmutableGameList();
-//    }
-//
-////    TODO: Implement User Authentification in Second Sprint
-////    UserData authenticateUser(String userName, String userPassword) throws IllegalAccessError{
-////
-////    }
-//
-//    // First Sprint Version
-//    // View calls this when user select a game to play
-//    // Input: Selected GameInfo Object
-//    // No Return Value
-//    public void createGameInstance(GameInfo selectedGame) {
-//        myGame = myGameLibrary.getGame(selectedGame);
-//    }
-//
-//    // TODO: Not completely necessary
-//    // This method would return the game objects description string to display in a Splash Screen
-////    public String getGameDescription(){
-////
-////    }
-//
-//
-//    // View calls this when the user presses play or level is over
-//    // No Input
-//    // Return: List of Viewable instances of static level items
-//    public List<ImmutableImageView> getLevelTerrain(){
+//     TODO: Second Sprint
+//     private UserAuthenticator myUserAuthenticator;
+//     private myUserGameData;
+
+    private Game myGame;
+    private GameLibrary myGameLibrary;
+
+    public Logic() {
+//        myUserAuthenticator = new UserAuthenticator();
+        myGameLibrary = new GameLibrary();
+
+    }
+
+
+    // View will call this first to get the name and thumbnail file name of each game
+    // No Input
+    // Return: List of GameInfo objects
+    public List<GameInfo> getGameOptions(){
+        return myGameLibrary.getImmutableGameList();
+    }
+
+//    TODO: Implement User Authentification in Second Sprint
+//    UserData authenticateUser(String userName, String userPassword) throws IllegalAccessError{
 //
 //    }
-//
-//    // View call this when the user presses play or a level is over
-//    // Return: ID and image file of available weapons
-//    public List<WeaponInfo> getArsenal(){
-//
-//    }
-//
-//    // View calls this when a weapon is placed onto the map
-//    // Input: WeaponInfo Object
-//    // Return: ImageView corresponding to the weapon
-//    public ImmutableImageView instantiateWeapon(WeaponInfo newWeapon){
-//
-//    }
-//
-//    // View calls to update the state of the Dynamic parts of the level in the game loop
-//    // Input: Time the method is called
-//    // No Return
-//    public void update(double currentTime){
+
+    // First Sprint Version
+    // View calls this when user select a game to play
+    // Input: Selected GameInfo Object
+    // No Return Value
+    public void createGameInstance(GameInfo selectedGame) {
+        myGame = myGameLibrary.getGame(selectedGame);
+    }
+
+    // TODO: Not completely necessary
+    // This method would return the game objects description string to display in a Splash Screen
+//    public String getGameDescription(){
 //
 //    }
-//
-//    // View calls to check if a location is valid to place a weapon
-//    // Input: WeaponInfo object, x and y coordinate
-//    // Return: boolean
-//    public boolean checkPlacementLocation(WeaponInfo movingWeapon, double x, double y){
+
+
+    // View calls this when the user presses play or level is over
+    // No Input
+    // Return: List of Viewable instances of static level items
+    public List<ImmutableImageView> getLevelTerrain(){
+
+    }
+
+    // View call this when the user presses play or a level is over
+    // Return: ID and image file of available weapons
+    public List<WeaponInfo> getArsenal(){
+
+    }
+
+    // View calls this when a weapon is placed onto the map
+    // Input: WeaponInfo Object
+    // Return: ImageView corresponding to the weapon
+    public ImmutableImageView instantiateWeapon(WeaponInfo newWeapon){
+
+    }
+
+    // View calls to update the state of the Dynamic parts of the level in the game loop
+    // Input: Time the method is called
+    // No Return
+    public void update(double currentTime){
+
+    }
+
+    // View calls to check if a location is valid to place a weapon
+    // Input: WeaponInfo object, x and y coordinate
+    // Return: boolean
+    public boolean checkPlacementLocation(WeaponInfo movingWeapon, double x, double y){
+
+    }
+
+    // View calls to move a dynamic object that has already been instantiated
+    // Input: WeaponInfo object, x and y coordinate
+    // No return
+    // TODO: Second Sprint move objects mid level
+//    public void placeMovingObject(WeaponInfo placedWeapon, double x, double y){
 //
 //    }
-//
-//    // View calls to move a dynamic object that has already been instantiated
-//    // Input: WeaponInfo object, x and y coordinate
-//    // No return
-//    // TODO: Second Sprint move objects mid level
-////    public void placeMovingObject(WeaponInfo placedWeapon, double x, double y){
-////
-////    }
-//
-//    // View calls this in game Loop to check if the level has ended
-//    // No input
-//    // Return: Boolean value indicating the status of the running level
-//    boolean checkIfLevelEnd(){
-//
-//        return false;
-//    }
+
+    // View calls this in game Loop to check if the level has ended
+    // No input
+    // Return: Boolean value indicating the status of the running level
+    boolean checkIfLevelEnd(){
+
+        return false;
+    }
 
 
 }
