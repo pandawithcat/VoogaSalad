261a9206d2028dcc487b20d4c99a2a8aaf59cbd8
Bryant Huang <bh179@duke.edu>
2019-04-08 09:30:40 -0400

    implement map

--- src/GamePlayer/GameView/src/Player/GamePlayMap.java
+++ src/GamePlayer/GameView/src/Player/GamePlayMap.java
@@ -1,17 +1,29 @@
 package Player;
 
 import BackendExternal.Logic;
+import BackendExternal.WeaponInfo;
+import Configs.ImmutableImageView;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class GamePlayMap extends GridPane {
+    private Logic myLogic;
+    private List <ImmutableImageView> terrainList;
+    private Group mapRoot;
 
     public GamePlayMap(double width, double height, Logic logic) {
-
-        setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 255), CornerRadii.EMPTY, Insets.EMPTY)));
+        myLogic = logic;
+        terrainList = myLogic.getLevelTerrain();
+        mapRoot = new Group();
+        //TODO: fix what the getLeveTerrain() method returns - we need objects that can be added to root
+        terrainList.forEach(terrainObject -> mapRoot.getChildren().addAll(terrainObject));
         setPrefWidth(width);
         setPrefHeight(height);
     }
+
 }
