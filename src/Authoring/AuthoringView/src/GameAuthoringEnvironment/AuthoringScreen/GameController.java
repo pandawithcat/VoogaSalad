2c388accc986507398cf630739bf3be43cc3b644
dl199@duke.edu <dl199@duke.edu>
2019-04-09 15:07:17 -0400

    sourview eventhandler added

--- src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringScreen/GameController.java
+++ src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringScreen/GameController.java
@@ -46,16 +46,17 @@ public class GameController {
         VBox layout = new VBox();
         for (String key : attributesMap.keySet()) {
             var value = attributesMap.get(key);
-            //System.out.println(key + " " + value);
+
             //handle primitives
             if(value.equals(java.lang.String.class) || value.equals(java.lang.Integer.class) || value.equals(java.lang.Boolean.class)){
                 Label myLabel = new Label(key);
                 TextField myTextField = new TextField();
-                layout.getChildren().addAll(myLabel, myTextField);
+                Button confirmButton = new Button("Confirm");
+                layout.getChildren().addAll(myLabel, myTextField, confirmButton);
             }
 
             else if(value.isInstance(Paths.class)){
-                System.out.println(value + " is "+ "paths class");
+
                 Button fileUploadButton = new Button("Upload Image");
                 layout.getChildren().add(fileUploadButton);
                 FileChooser fileChooser = new FileChooser();
@@ -71,14 +72,14 @@ public class GameController {
             //handle single object
 
             else if(!value.isArray() && value.getClass().isInstance(Configuration.class)){
-                System.out.println(key + " " + value);
+
                 Button myButton = new Button("Configure " + key);
                 myButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent event) {
                         try {
                             Object myObject = value.getConstructor().newInstance();
-                            System.out.println(myObject.toString() + "single object");
+
                             if (myObject instanceof Configurable) {
                                 createConfigurable((Configurable) myObject);
                             }
@@ -92,30 +93,34 @@ public class GameController {
             //handle list
             else if(value.isArray()) {
                 if(value.getComponentType().getClass().isInstance(Configurable.class)) {
+                   /* System.out.println(value);
+                    System.out.println(value.getComponentType());
+                    System.out.println(value.getComponentType().getName());*/
                     VBox tempVBOx  = new VBox();
                     Button addNew = new Button("add new " + key);
                     ListView sourceView = new ListView<>();
+
+                    //make new screen pop up when each level clicked
+
                     sourceView.setOnMouseClicked((new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent event) {
-                            createConfigurable((Configurable) sourceView.getSelectionModel().getSelectedItems());
+                            try {
+                                Object object = Class.forName(value.getComponentType().getName());
+                                System.out.println(object.toString());
+                                createConfigurable((Configurable) object);
+                            } catch (Exception e){
+
+                            }
                         }
                     }));
                     addNew.setOnMouseClicked((new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent event) {
                             sourceView.getItems().add(key);
+
                         }
                     }));
-
-                    /*try {
-                        Class myClass = value.getComponentType().getClass();
-                        Object myObject = value.getComponentType().getConstructor().newInstance();
-                        List<?> generocList;
-                        Object[] list = (Object[]) Array.newInstance(myObject.getClass());
-                    }catch (Exception e){
-
-                    }*/
 
                     tempVBOx.getChildren().addAll(sourceView, addNew);
                     layout.getChildren().add(tempVBOx);
