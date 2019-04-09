567aa63f2a13cea191a009e998d98303dfa1ef72
Bryant Huang <bh179@duke.edu>
2019-04-08 21:28:18 -0400

    terrain add done

--- src/GamePlayer/GameView/src/Player/GamePlayMap.java
+++ src/GamePlayer/GameView/src/Player/GamePlayMap.java
@@ -3,21 +3,22 @@ package Player;
 import BackendExternal.Logic;
 import Configs.TransferImageView;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.layout.*;
 
 import java.util.List;
 
 public class GamePlayMap extends GridPane {
     private Logic myLogic;
-    private List <TransferImageView> terrainList;
+    private List terrainList;
     private Group mapRoot;
 
     public GamePlayMap(double width, double height, Logic logic) {
         myLogic = logic;
         terrainList = myLogic.getLevelTerrain();
         mapRoot = new Group();
-        //TODO: fix what the getLevelTerrain() method returns - we need objects that can be added to root
-        terrainList.forEach(terrainObject -> mapRoot.getChildren().addAll(terrainObject));
+        //TODO: not sure if this works yet
+        terrainList.forEach(terrainNode -> mapRoot.getChildren().add((Node)terrainNode));
         setPrefWidth(width);
         setPrefHeight(height);
     }
