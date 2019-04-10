cba2525893bd11232d7e95a7f302ccc6b2f302a0
dl199@duke.edu <dl199@duke.edu>
2019-04-09 21:29:48 -0400

    gamecontroller tree done

--- src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringScreen/GameController.java
+++ src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringScreen/GameController.java
@@ -20,10 +20,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class GameController {
@@ -37,6 +34,7 @@ public class GameController {
 
 
     public void createConfigurable(Configurable myConfigurable){
+
         Stage popupwindow = new Stage();
 
         popupwindow.initModality(Modality.APPLICATION_MODAL);
@@ -45,7 +43,7 @@ public class GameController {
 
         Map<String, Object> myAttributesMap = new HashMap<>();
         Map<String, Class> attributesMap = myConfigurable.getConfiguration().getAttributes();
-        System.out.println(attributesMap);
+
         VBox layout = new VBox(10.00);
         layout.autosize();
         for (String key : attributesMap.keySet()) {
@@ -57,7 +55,6 @@ public class GameController {
             }
 
             //handle primitives
-            System.out.println("Check Primitive" + value.isPrimitive());
             if(value.equals(java.lang.String.class) || value.equals(java.lang.Integer.class) || value.equals(java.lang.Boolean.class) || value.isPrimitive()){
                 Label myLabel = new Label(key);
                 TextField myTextField = new TextField();
@@ -115,21 +112,34 @@ public class GameController {
             //handle list
             else if(value.isArray()) {
                 if(value.getComponentType().getClass().isInstance(Configurable.class)) {
-
+                    List<Object> tempList = new ArrayList<>();
+                    Label listLabel = new Label("Add new " + value.getComponentType().getSimpleName() + " here");
                     VBox tempVBOx  = new VBox();
+                    tempVBOx.setSpacing(10);
                     var buttonBar = new HBox();
+                    buttonBar.setSpacing(10);
                     Button addNew = new Button("add new " + value.getComponentType().getSimpleName());
                     Button confirmButton = new Button("Confirm");
                     buttonBar.getChildren().addAll(addNew, confirmButton);
                     ListView sourceView = new ListView<>();
-                    confirmButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
+
+                    addNew.setOnMouseClicked((new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent event) {
-                            myAttributesMap.put(key, sourceView.getItems());
+                            sourceView.getItems().add(value.getComponentType().getSimpleName());
+                            try {
+                                Class<?> cl = Class.forName(value.getComponentType().getName());
+                                Constructor<?> cons = cl.getConstructor(myConfigurable.getClass());
+                                var object = cons.newInstance(myConfigurable);
+                                tempList.add(object);
+                                System.out.println(tempList.get(0).getClass());
+
+                            } catch (Exception e) {
+
+                            }
+
                         }
                     }));
-
-                    //make new screen pop up when each level clicked
 
                     sourceView.setOnMouseClicked((new EventHandler<MouseEvent>() {
 
@@ -149,15 +159,15 @@ public class GameController {
                             }
                         }
                     }));
-                    addNew.setOnMouseClicked((new EventHandler<MouseEvent>() {
+
+                    confirmButton.setOnMouseClicked((new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent event) {
-                            sourceView.getItems().add(value.getComponentType().getSimpleName());
-
+                            myAttributesMap.put(key, tempList);
                         }
                     }));
 
-                    tempVBOx.getChildren().addAll(sourceView, buttonBar);
+                    tempVBOx.getChildren().addAll(listLabel, sourceView, buttonBar);
                     layout.getChildren().add(tempVBOx);
                 }
             }
