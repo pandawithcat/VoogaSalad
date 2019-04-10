ca606824a57afb20b1541bd3c10fdab783b7ca0a
Kunal Upadya <ku16@duke.edu>
2019-04-10 00:55:44 -0400

    changed configurable

--- src/Configurations/src/Configs/ArsenalConfig/WeaponBehaviors/WeaponBehavior.java
+++ src/Configurations/src/Configs/ArsenalConfig/WeaponBehaviors/WeaponBehavior.java
@@ -3,8 +3,18 @@ package Configs.ArsenalConfig.WeaponBehaviors;
 import Configs.ArsenalConfig.WeaponConfig;
 import Configs.Behaviors.Behavior;
 
+import java.util.List;
+
 public abstract class WeaponBehavior implements Behavior<WeaponConfig> {
     WeaponConfig myWeaponConfig;
+    public static final List<Class> IMPLEMENTING_BEHAVIORS = List.of(
+            AmmoExpirable.class,
+            HealthExpirable.class,
+            Movable.class,
+            PlaceableOnPath.class,
+            Shootable.class,
+            TimeExpirable.class);
+
     WeaponBehavior(WeaponConfig weaponConfig){
         myWeaponConfig = weaponConfig;
     }
--- src/Configurations/src/Configs/ProjectilePackage/ProjectileBehaviors/ProjectileBehavior.java
+++ src/Configurations/src/Configs/ProjectilePackage/ProjectileBehaviors/ProjectileBehavior.java
@@ -2,8 +2,12 @@ package Configs.ProjectilePackage.ProjectileBehaviors;
 
 import Configs.ProjectilePackage.ProjectileConfig;
 
+import java.util.List;
+
 public abstract class ProjectileBehavior {
     ProjectileConfig myProjectileConfig;
+    public static final List<Class> IMPLEMENTING_BEHAVIORS = List.of();
+
     ProjectileBehavior(ProjectileConfig projectileConfig){
         myProjectileConfig = projectileConfig;
     }
