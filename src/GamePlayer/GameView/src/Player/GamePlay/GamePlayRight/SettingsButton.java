1c08cadffc6b015f14d6e7929178bf5d640a4115
Bryant Huang <bh179@duke.edu>
2019-04-22 15:19:40 -0400

    slider

--- src/GamePlayer/GameView/src/Player/GamePlay/GamePlayRight/SettingsButton.java
+++ src/GamePlayer/GameView/src/Player/GamePlay/GamePlayRight/SettingsButton.java
@@ -1,9 +1,14 @@
 package Player.GamePlay.GamePlayRight;
 
-import javafx.scene.control.Button;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 public class SettingsButton extends Button {
     private String icon = "settings.png";
@@ -19,7 +24,29 @@ public class SettingsButton extends Button {
         setOnAction(e -> displaySettings());
     }
 
-    private Popup displaySettings(){
-        return new Popup();
+    private Dialog displaySettings(){
+        Dialog dialog = new Dialog();
+        dialog.setTitle("Game Settings");
+        dialog.setHeaderText("Adjust Game Settings Below");
+        DialogPane dialogPane = dialog.getDialogPane();
+        TextField musicInput = new TextField();
+        TextField soundInput = new TextField();
+        Slider sound = new Slider(0,1,0.5);
+        sound.setShowTickLabels(true);
+        sound.setShowTickMarks(true);
+        sound.setBlockIncrement(10);
+        Label l = new Label("Change the Music");
+        sound.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    public void changed(ObservableValue<? extends Number >
+                                                observable, Number oldValue, Number newValue)
+                    {
+
+                        l.setText("Sound: " + newValue);
+                    }
+                });
+        dialogPane.setContent(new VBox(20, musicInput,soundInput,sound));
+        dialog.showAndWait();
+        return dialog;
     }
 }
