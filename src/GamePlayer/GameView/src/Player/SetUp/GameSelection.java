24c7f1bb26be6da99578194e4525dffc88cda651
Mark Kang <sk531@duke.edu>
2019-04-23 14:43:27 -0400

    added a new game selection screen

--- src/GamePlayer/GameView/src/Player/SetUp/GameSelection.java
+++ src/GamePlayer/GameView/src/Player/SetUp/GameSelection.java
@@ -2,16 +2,20 @@ package Player.SetUp;
 
 import BackendExternal.GameInfo;
 import BackendExternal.Logic;
+import Player.GamePlay.GamePlayMain;
 import Player.ScreenSize;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,29 +27,32 @@ public class GameSelection extends Application {
 
     public static final String RESOURCES_PATH = "resources/";
 
-    private VBox root;
+    private HBox root;
     private Stage stage;
     private ScrollPane scrollPane = new ScrollPane();
     private double width = ScreenSize.getWidth();
     private double height = ScreenSize.getHeight();
     private Logic logic;
+    private StackPane gameStart;
     @Override
     public void start(Stage primaryStage) {
         scrollPane.setId("scrollpane");
-        scrollPane.setPrefSize(width/2,height/2);
+        scrollPane.setPrefSize(width/3,height);
+        scrollPane.setOpacity(0.8);
         logic = new Logic();
         stage = primaryStage;
         stage.setX(width);
         stage.setY(height);
-        root = new VBox();
-        root.setAlignment(Pos.CENTER);
+        root = new HBox();
         root.setId("pane");
         Label text = new Label("Select a Game");
         text.setPrefHeight(100);
         text.setId("selectGame");
-        root.getChildren().add(text);
+        VBox left = new VBox();
+        left.getChildren().add(text);
+        left.getChildren().add(scrollPane);
+        root.getChildren().add(left);
         text.setLayoutX(ScreenSize.getWidth()/2);
-        root.getChildren().add(scrollPane);
         var scene = new Scene(root, width, height);
         scene.getStylesheets().add("style.css");
         primaryStage.setScene(scene);
@@ -64,28 +71,61 @@ public class GameSelection extends Application {
     private void createGameSelectionScreen() throws FileNotFoundException {
         VBox vBox = new VBox();
         vBox.setPrefWidth(width/3);
-            HBox hBox = new HBox();
-            hBox.setPrefWidth(width/3);
-            for(GameInfo gameInfo: uploadAvailableGames()){
-                Text title = new Text(gameInfo.getGameTitle());
-                title.setX(300);
-                hBox.getChildren().add(title);
-                Image image = new Image(new FileInputStream(RESOURCES_PATH + gameInfo.getGameThumbnail()));
-                ImageView imageView = new ImageView(image);
-                imageView.setFitWidth(100);
-                imageView.setFitHeight(100);
-                imageView.setX(500);
-                imageView.setOnMouseClicked(e -> startGame(gameInfo, image));
-                hBox.getChildren().add(imageView);
-            }
-            vBox.getChildren().add(hBox);
-
-        scrollPane.setContent(hBox);
+        vBox.setAlignment(Pos.CENTER);
+        for(GameInfo gameInfo: uploadAvailableGames()){
+            Rectangle container = new Rectangle();
+            Text title = new Text(gameInfo.getGameTitle());
+            title.setX(300);
+            vBox.getChildren().add(title);
+            Image image = new Image(new FileInputStream(RESOURCES_PATH + gameInfo.getGameThumbnail()));
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(100);
+            imageView.setX(500);
+            imageView.setOnMouseClicked(e -> startGame(gameInfo, image));
+            vBox.getChildren().add(imageView);
+        }
+        scrollPane.setContent(vBox);
     }
     private void startGame(GameInfo gameInfo, Image image){
+        setTitle(gameInfo, image);
+    }
+    public void setTitle(GameInfo gameInfo, Image image){
+        if(root.getChildren().contains(gameStart)){
+            root.getChildren().remove(gameStart);
+        }
+        gameStart = new StackPane();
+        gameStart.setAlignment(Pos.CENTER);
+        Text title = new Text(gameInfo.getGameTitle());
+        title.setTranslateY(-100);
+        Text subtitle = new Text(gameInfo.getGameDescription());
+        subtitle.setTranslateY(-60);
+        gameStart.getChildren().add(title);
+        gameStart.getChildren().add(subtitle);
+        gameStart.applyCss();
+        gameStart.layout();
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
+        gameStart.getChildren().add(imageView);
+        Button play = new Button("Play");
+        play.setTranslateX(0);
+        play.setTranslateY(100);
+        gameStart.getChildren().add(play);
+        play.setOnAction(e-> startGame(gameInfo));
+        gameStart.setPrefWidth(width* 2 /3);
+        root.getChildren().add(gameStart);
+    }
+    private void startGame(GameInfo gameInfo){
         this.stage.close();
-        GameStart gameStart = new GameStart();
-        gameStart.start(new Stage());
-        gameStart.setTitle(gameInfo, image);
+        LogInPreloader logInPreloader = new LogInPreloader();
+        logInPreloader.start(new Stage());
+        logInPreloader.setTitle("Get Ready to Play!");
+        logInPreloader.setTransitionEvent(e->transitionToScreen(gameInfo));
+    }
+    private void transitionToScreen(GameInfo gameInfo){
+        GamePlayMain gamePlayMain = new GamePlayMain();
+        gamePlayMain.setGameInfo(gameInfo);
+        gamePlayMain.start(new Stage());
     }
 }
