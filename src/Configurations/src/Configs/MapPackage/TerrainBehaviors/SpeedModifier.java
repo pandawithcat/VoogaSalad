33be7381825bdd2ad3ef52ef16f57e1162645d1b
Rishiraj Tripathy <rt125@duke.edu>
2019-04-27 15:29:39 -0400

    added skeleton for terrain and projectile behaviors

--- src/Configurations/src/Configs/MapPackage/TerrainBehaviors/SpeedModifier.java
+++ src/Configurations/src/Configs/MapPackage/TerrainBehaviors/SpeedModifier.java
@@ -1,4 +1,29 @@
 package Configs.MapPackage.TerrainBehaviors;
 
-public class SpeedModifier {
+import Configs.Configuration;
+import Configs.MapPackage.Terrain;
+import Configs.Updatable;
+
+public class SpeedModifier extends TerrainBehavior{
+    private transient Configuration myConfiguration;
+
+    private final String DISPLAY_LABEL = "Modify Enemy Speed";
+    public SpeedModifier(Terrain terrain){
+        super(terrain);
+        myConfiguration = new Configuration(this);
+    }
+    @Override
+    public void update(double ms, Updatable parent) {
+        //TODO
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return myConfiguration;
+    }
+
+    @Override
+    public String getName() {
+        return DISPLAY_LABEL;
+    }
 }
--- src/Configurations/src/Configs/ProjectilePackage/ProjectileBehaviors/Homing.java
+++ src/Configurations/src/Configs/ProjectilePackage/ProjectileBehaviors/Homing.java
@@ -1,4 +1,29 @@
 package Configs.ProjectilePackage.ProjectileBehaviors;
 
-public class Homing {
+import Configs.Configuration;
+import Configs.ProjectilePackage.ProjectileConfig;
+import Configs.Updatable;
+
+public class Homing extends ProjectileBehavior{
+    public static final String DISPLAY_LABEL = "Homing Behavior";
+    public transient Configuration myConfiguration;
+    public Homing(ProjectileConfig pc){
+        super(pc);
+        myConfiguration = new Configuration(this);
+    }
+    @Override
+    public String getName() {
+        return DISPLAY_LABEL;
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return myConfiguration;
+    }
+
+    @Override
+    public void update(double ms, Updatable parent) {
+
+    }
+
 }
