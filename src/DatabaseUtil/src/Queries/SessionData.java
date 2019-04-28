e9e6fe53bffef77de8b871f41ce4823b4b323850
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-04-28 04:25:48 -0400

    Added SessionData

--- src/DatabaseUtil/src/Queries/SessionData.java
+++ src/DatabaseUtil/src/Queries/SessionData.java
@@ -1,7 +1,8 @@
+package Queries;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class SessionData extends DBUtil{
