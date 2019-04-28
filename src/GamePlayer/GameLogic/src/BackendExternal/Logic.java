d850506664f6473b5b8f0a51d2a13f0bf903e36e
Bryant Huang <bh179@duke.edu>
2019-04-28 19:44:58 -0400

    database

--- src/GamePlayer/GameLogic/src/BackendExternal/Logic.java
+++ src/GamePlayer/GameLogic/src/BackendExternal/Logic.java
@@ -45,7 +45,7 @@ public class Logic {
 
     public Logic(double paneWidth, double paneHeight) {
         myGameLibrary = new GameLibrary();
-    //        myPlayerData = new PlayerData();
+            myPlayerData = new PlayerData();
         PANE_WIDTH = paneWidth;
         PANE_HEIGHT = paneHeight;
     }
