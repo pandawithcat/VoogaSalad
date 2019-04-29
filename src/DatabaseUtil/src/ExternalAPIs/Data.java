04a442a001c272ccb19a0c79227c31f5a2218964
Brian Jordan <bjj17@duke.edu>
2019-04-28 20:29:53 -0400

    Changed getImage to static

--- src/DatabaseUtil/src/ExternalAPIs/Data.java
+++ src/DatabaseUtil/src/ExternalAPIs/Data.java
@@ -15,7 +15,7 @@ import static Internal.Authentication.*;
 
 public abstract class Data {
 
-    private final int MAX_LOGIN_ATTEMPTS = 3;
+    private final int MAX_LOGIN_ATTEMPTS = 4;
 
     protected int currentUserID;
     protected int currentGameID;
@@ -117,7 +117,7 @@ public abstract class Data {
      * @param imageID - integer value corresponding to the specific image in the database
      * @return - byte array of requested image
      */
-    public byte[] getImage(int imageID){
+    public static byte[] getImage(int imageID){
         return getImage(imageID);
     }
 
