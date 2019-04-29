99897e7e462acbef76f22f1537e0d016c01b9926
Mark Kang <sk531@duke.edu>
2019-04-28 23:07:33 -0400

    authoring side pulling image from database

--- src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringComponents/ConfigureImage.java
+++ src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringComponents/ConfigureImage.java
@@ -5,15 +5,22 @@ import Configs.Configurable;
 import ExternalAPIs.AuthoringData;
 import GameAuthoringEnvironment.AuthoringScreen.GameController;
 import GameAuthoringEnvironment.AuthoringScreen.ImageBox;
+import GameAuthoringEnvironment.AuthoringScreen.ScreenSize;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -24,7 +31,7 @@ import java.util.Map;
 
 
 public class ConfigureImage {
-
+    private Text configuredPrompt;
     List<Class> myList;
     Map<String, Object> myMap;
     Stage popUpWindow;
@@ -43,22 +50,17 @@ public class ConfigureImage {
     }
     private void addImageID(Integer integer){
         myTextField.setText(Integer.toString(integer));
+        configuredPrompt.setText("Your Image has been Configured!");
     }
 
     private void setContent(){
+        configuredPrompt = new Text("");
+        configuredPrompt.setFont(Font.font("Veranda", FontWeight.BOLD, 50));
         popUpWindow = new Stage();
         //popUpWindow.initModality(Modality.APPLICATION_MODAL);
         popUpWindow.setTitle("Image Editor");
         layout = new VBox(10.00);
         Model model = new Model();
-
-        Label imageLabel = new Label("Selected Image");
-        TextField imageTf = new TextField();
-
-        VBox nameVbox = new VBox(10);
-        nameVbox.getChildren().addAll(imageLabel, imageTf);
-
-        VBox imageVbox = new VBox(10);
 
         //TODO CHANGE This
         //List<Integer> allImages = model.getImageOptions(AuthoringData.ImageType.TERRAIN);
@@ -68,39 +70,20 @@ public class ConfigureImage {
             allImagesList.add(model.getImage(allImages.get(a)));
         }
         for(int x = 0; x< allImages.size(); x ++){
-            ImageView imageView = new ImageView(allImagesList.get(x));
+            Image image = allImagesList.get(x);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(ScreenSize.getWidth()/3);
+            imageView.setFitHeight(ScreenSize.getHeight()/3);
             layout.getChildren().add(imageView);
             Integer id = allImages.get(x);
             imageView.setOnMousePressed(e->addImageID(id));
         }
-
-        for(int a=0; a< allImages.size(); a++) {
-
-            ImageView imageView = new ImageView(allImagesList.get(a));
-            imageView.setFitWidth(20);
-            imageView.setFitHeight(20);
-            imageVbox.getChildren().add(imageView);
-            imageView.setOnMouseClicked((new EventHandler<MouseEvent>() {
-
-                @Override
-                public void handle(MouseEvent event) {
-                    if (event.getButton().equals(MouseButton.PRIMARY)) {
-                        if (event.getClickCount() == 2) {
-
-                            imageTf.setText("");
-                            myTextField.setText("");
-                            }
-
-                        }
-
-                }
-            }));
-            imageVbox.getChildren().add(imageView);
-        }
-
-
-        layout.getChildren().addAll(nameVbox, imageVbox);
-        Scene scene= new Scene(layout, 800, 800);
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(layout);
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getChildren().addAll(sp, configuredPrompt);
+        Scene scene= new Scene(hBox, 800, 800);
         popUpWindow.setScene(scene);
         popUpWindow.show();
     }
