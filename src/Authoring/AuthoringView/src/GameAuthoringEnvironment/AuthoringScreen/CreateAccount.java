8153cd058a6518c9bfa68b0aae521b8c67b41d66
Hyunjae Lee <hl285@duke.edu>
2019-04-29 02:15:29 -0400

    some error checking for user authentication and error checking

--- src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringScreen/CreateAccount.java
+++ src/Authoring/AuthoringView/src/GameAuthoringEnvironment/AuthoringScreen/CreateAccount.java
@@ -7,6 +7,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -68,14 +69,20 @@ public class CreateAccount extends Application {
     }
 
     private void handleMakeAccountButton(MouseEvent event){
-        if(pwTf.getText().equals(confirmPwTf.getText())){
-            //TODO USE regex to check the id, pw etc
-            myModel.createNewUser(newIDTf.getText(), pwTf.getText(), confirmPwTf.getText());
-            popupwindow.close();
+        try {
+            if (pwTf.getText().equals(confirmPwTf.getText())) {
+                //TODO USE regex to check the id, pw etc
+                myModel.createNewUser(newIDTf.getText(), pwTf.getText(), confirmPwTf.getText());
+                popupwindow.close();
+            }
         }
-        else{
-            //TODO Create Alert
+        catch(Exception e){
+            Alert alert= new Alert(Alert.AlertType.WARNING,e.getMessage());
+            alert.showAndWait();
         }
+//        else{
+//            //TODO Create Alert
+//        }
     }
 
 }
